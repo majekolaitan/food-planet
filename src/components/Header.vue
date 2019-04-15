a9bf41abe59df29063b9f6bd72c91a9bf51d74ba
majekolaitan <majekolaitan@gmail.com>
2019-04-15 16:39:42 +0400

    Video 1 - 13

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="row">
+        <nav class="navbar navbar-toggleable-md navbar-light">
+            <a class="navbar-brand" href="/">FOOD PLANET</a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Menu</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style>
+header {
+    margin-bottom: 20px;
+}
+
+.navbar-brand {
+    font-size: 1.5em;
+}
+</style>

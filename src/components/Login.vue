06b9c1959215cab1873dee80d3442ea1b4cca059
majekolaitan <majekolaitan@gmail.com>
2019-04-16 10:05:04 +0400

    22. Enable Firebase authentication - 23. Signing in and out

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,8 +9,40 @@
           <label>Password</label>
           <input type="password" class="form-control" id="password" placeholder="Enter password">
         </div>
-        <button type="button" class="btn btn-primary">Sign in</button>
-        <button type="button" class="btn btn-danger">Sign out</button>
+        <button type="button" class="btn btn-primary" @click.prevent="signIn">Sign in</button>
+        <button type="button" class="btn btn-danger" @click.prevent="signOut">Sign out</button>
       </form>
   </div>
 </template>
+
+<script>
+import Firebase from 'firebase'
+
+export default {
+  methods: {
+    signIn() {
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+
+      Firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if(errorCode === 'auth/wrong-password' ) {
+          alert('Wrong password')
+        } else {
+          alert(errorMessage);
+        }
+      });
+    },
+    signOut() {
+      Firebase.auth().signOut().then(function() {
+        alert('logged out');
+      }).catch(function(error) {
+        alert('error.');
+      })
+    }
+  }
+}
+
+</script>

1256fa03d424b71e7846c637894aa4c4a6e5811f
majekolaitan <majekolaitan@gmail.com>
2019-04-17 07:17:44 +0400

    66. Global currency filter

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import { routes } from './routes'
 import { store } from './store/store.js'
+import Accounting from 'accounting-js'
 import App from './App.vue'
 
 Vue.use(VueRouter)
@@ -9,6 +10,10 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   routes,
   mode: 'history'
+})
+
+Vue.filter('currency', function(val){
+  return Accounting.formatMoney(val)
 })
 
 new Vue({
